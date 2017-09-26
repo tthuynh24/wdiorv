8cd5e0fb5012ee26a9130eb7d7f0c5347ca0e85e
tthuynh24 <tthuynh24@outlook.com>
2017-09-26 03:18:26 -0400

    added price and numbers test

--- test/specs/rvtest.js
+++ test/specs/rvtest.js
@@ -29,13 +29,13 @@ describe('making sure links direct correctly', () => {
     })
 
     //Shop Online Page
-    it.only('should have a link to the Shop Online page', () => {
+    it('should have a link to the Shop Online page', () => {
         var shopOnlinePage = browser.isExisting('=Shop Online')
         assert(shopOnlinePage);
     })
 
     // Ended up to a page that has no title so test failed
-    it.only('should take you to the Shop Online page', () => {
+    it('should take you to the Shop Online page', () => {
         browser.click('=Shop Online')
         var title = browser.getTitle();
         assert.equal(title, 'https://internet.frontier.com/cart/address')
@@ -43,26 +43,37 @@ describe('making sure links direct correctly', () => {
 });
 
 describe('image/graphic visibility test', () => {
-
     beforeEach(function () {	    
 	    browser.url('/');
 	    browser.pause(2000);
-	});
+	})
 
-	
 	it('should have a visible logo image', () => {
 		var logoImg = browser.isVisible('.masthead__logo');
 		console.log("Logo image is visible:", logoImg);
 		expect(logoImg).to.eq(true);
-	});
+	})
 
 	it('should detect main nav bar is visible ', () => {
 	    var navBar = browser.isVisible('.nav-aux');
 	    console.log("Nav bar is visible:", navBar);
 	    expect(navBar).to.eq(true);
-	});
+	})
+});
 
-	
-	
+describe('prices and phone numbers are formatted correctly', () => {
+    beforeEach(function () {
+        browser.url('/');
+        browser.pause(2000);
+    });
+
+    it.only('should show prices in the right format', () => {
+        var priceLine = browser.getText('body > main > section.hero.hero--home.hero--tall > div > div > div > div.medium-5.large-12.columns > div > p')
+        var price = priceLine.substring(21, 26);
+        const regex = /(\d+\d{1,2})/;
+        console.log(price);
+        expect(regex.test(price)).to.be.true;
+    })
+
 
 });
